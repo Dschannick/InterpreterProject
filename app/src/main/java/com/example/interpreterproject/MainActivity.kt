2ca3ea04a85c51ccf389f6f4f195831d989c960b
GERYJU <yannick_jungmann@qvc.com>
2021-12-17 11:01:50 +0100

    T4 first try of the interpreter stucture

--- app/src/main/java/com/example/interpreterproject/MainActivity.kt
+++ app/src/main/java/com/example/interpreterproject/MainActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import com.example.rte.TestAccess
+import com.test_pack.TestAccessCoreLib
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Yannick", TestAccess().testString)
-        findViewById<TextView>(R.id.showTextField).text = TestAccess().testString
+        Log.d("Yannick", TestAccess().testString + " X " + TestAccessCoreLib().testString)
+        findViewById<TextView>(R.id.showTextField).text = TestAccess().testString + " X " + TestAccessCoreLib().testString
     }
 }
