75242a4ee0f287d1de1a7ca6c93f526995d7eb2f
qvcdev\geryju <YANNICK_JUNGMANN@QVC.COM>
2023-08-18 11:08:11 +0200

    geo and product flavers

--- app/src/main/java/com/example/interpreterproject/MainActivity.kt
+++ app/src/main/java/com/example/interpreterproject/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.interpreterproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var tw = findViewById<TextView>(R.id.mainText)
+        tw.text = "set text"
     }
 }
